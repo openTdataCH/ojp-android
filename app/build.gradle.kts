9370ee5c6fee8d33f5d1e95c2e3f8fb366c07f5c
deka91 <denkal91@googlemail.com>
2024-04-30 10:26:26 +0200

    Add new version declaration in version catalog for compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
     packaging {
         resources {
