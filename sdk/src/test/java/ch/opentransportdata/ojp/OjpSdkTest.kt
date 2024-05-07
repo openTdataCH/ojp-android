2da03dbd422b4f7431514a8757448a55895cfbe6
deka91 <denkal91@googlemail.com>
2024-05-07 16:03:10 +0200

    Implement unit tests for requestLocationsFromSearchTerm

---
+++ sdk/src/test/java/ch/opentransportdata/ojp/OjpSdkTest.kt
@@ -0,0 +1,86 @@
+package ch.opentransportdata.ojp
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import assertk.assertions.isNotEmpty
+import ch.opentransportdata.ojp.data.dto.OjpDto
+import ch.opentransportdata.ojp.domain.model.PlaceTypeRestriction
+import ch.opentransportdata.ojp.domain.model.Result
+import ch.opentransportdata.ojp.domain.model.error.OjpError
+import com.tickaroo.tikxml.TikXml
+import kotlinx.coroutines.test.runTest
+import okio.buffer
+import okio.source
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
+
+/**
+ * Created by Deniz Kalem on 07.05.2024
+ */
+internal class OjpSdkTest {
+
+    private val invalidXmlFile = """
+            <xml>
+                <field1>Value1</field1>
+                <field2>Value2</field2>
+            </xml>
+        """.trimIndent()
+
+//    @Test
+//    fun `Invalid XML should throw an exception`() {
+//        val inputStream: InputStream = ByteArrayInputStream(invalidXmlFile.toByteArray())
+//        val bufferedSource = inputStream.source().buffer()
+//        val tikXml = TikXml.Builder().exceptionOnUnreadXml(false).build()
+//
+//        tikXml.read<OjpDto>(bufferedSource, OjpDto::class.java)
+//    }
+
+    @Test
+    fun `requestLocationsFromSearchTerm with valid data should return a list of places`() {
+        runTest {
+            // GIVEN
+            val ojpSdk = OjpSdk(
+                baseUrl = "https://odpch-api.clients.liip.ch/",
+                endpoint = "ojp20-beta",
+                httpHeaders = hashMapOf("Authorization" to "Bearer eyJvcmciOiI2M2Q4ODhiMDNmZmRmODAwMDEzMDIwODkiLCJpZCI6IjUzYzAyNWI2ZTRhNjQyOTM4NzMxMDRjNTg2ODEzNTYyIiwiaCI6Im11cm11cjEyOCJ9"),
+                requesterReference = "OJP_Demo"
+            )
+            val term = "Bern"
+
+            // ACTION
+            val result = ojpSdk.requestLocationsFromSearchTerm(
+                term = term, restrictions = listOf(PlaceTypeRestriction.STOP, PlaceTypeRestriction.TOPOGRAPHIC_PLACE)
+            )
+
+            // ASSERTION
+            assertThat(result).isInstanceOf(Result.Success::class.java)
+            assertThat((result as Result.Success).data).isNotEmpty()
+        }
+    }
+
+    @Test
+    fun `requestLocationsFromSearchTerm with empty bearer should return UNEXPECTED_HTTP_STATUS`() {
+        runTest {
+            // GIVEN
+            val ojpSdk = OjpSdk(
+                baseUrl = "https://odpch-api.clients.liip.ch/",
+                endpoint = "ojp20-beta",
+                httpHeaders = hashMapOf("Authorization" to ""),
+                requesterReference = "OJP_Demo"
+            )
+            val term = "Bern"
+
+            // ACTION
+            val result = ojpSdk.requestLocationsFromSearchTerm(
+                term = term, restrictions = listOf(PlaceTypeRestriction.STOP, PlaceTypeRestriction.TOPOGRAPHIC_PLACE)
+            )
+
+            // ASSERTION
+            assertThat(result).isInstanceOf(Result.Error::class.java)
+            assertThat((result as Result.Error).error).isEqualTo(OjpError.UNEXPECTED_HTTP_STATUS)
+        }
+    }
+}
