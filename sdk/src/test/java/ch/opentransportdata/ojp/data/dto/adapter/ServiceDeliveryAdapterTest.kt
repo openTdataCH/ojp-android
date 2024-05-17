ec9d7bdfdabd53c2af6f76a2d158e608b5f9d59c
kalevan <michi.ruppen@gmail.com>
2024-05-17 15:00:01 +0200

    Implement tests for ServiceDeliveryAdapter

---
+++ sdk/src/test/java/ch/opentransportdata/ojp/data/dto/adapter/ServiceDeliveryAdapterTest.kt
@@ -0,0 +1,79 @@
+package ch.opentransportdata.ojp.data.dto.adapter
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import assertk.assertions.isGreaterThan
+import assertk.assertions.isNotNull
+import ch.opentransportdata.ojp.TestUtils
+import ch.opentransportdata.ojp.data.dto.converter.PtModeTypeConverter
+import ch.opentransportdata.ojp.data.dto.response.PlaceDto
+import ch.opentransportdata.ojp.data.dto.response.ServiceDeliveryDto
+import ch.opentransportdata.ojp.data.dto.response.delivery.LocationInformationDeliveryDto
+import ch.opentransportdata.ojp.domain.model.PtMode
+import com.tickaroo.tikxml.TikXml
+import okio.Buffer
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+/**
+ * Created by Michael Ruppen on 17.05.2024
+ */
+class ServiceDeliveryAdapterTest {
+
+    private val tikXml = TikXml.Builder()
+        .addTypeAdapter(ServiceDeliveryDto::class.java, ServiceDeliveryAdapter())
+        .addTypeAdapter(PlaceDto::class.java, PlaceAdapter())
+        .addTypeConverter(PtMode::class.java, PtModeTypeConverter())
+        .build()
+
+    @Test
+    fun `Delivery type LocationInformation, parsing successful`() {
+        // GIVEN
+        val xmlFile = "src/test/resources/adapter/service/location_information_delivery.xml"
+        val bufferedSource = TestUtils().readXmlFile(xmlFile)
+
+        // ACTION
+        val result = tikXml.read<ServiceDeliveryDto>(bufferedSource, ServiceDeliveryDto::class.java)
+
+        // ASSERTION
+        assertThat(result).isNotNull()
+        assertThat(result.responseTimestamp).isEqualTo("2024-04-12T13:56:49.2188513+02:00")
+        assertThat(result.producerRef).isEqualTo("MENTZ")
+        assertThat(result.ojpDelivery.javaClass).isEqualTo(LocationInformationDeliveryDto::class.java)
+
+        val delivery = result.ojpDelivery as LocationInformationDeliveryDto
+        assertThat(delivery.placeResults).isNotNull()
+        assertThat(delivery.placeResults.isNullOrEmpty()).isFalse()
+    }
+
+    @Test
+    fun `No delivery object available, parsing failed`() {
+        // GIVEN
+        val xmlFile = "src/test/resources/adapter/service/no_delivery.xml"
+        val bufferedSource = TestUtils().readXmlFile(xmlFile)
+
+        // ACTION
+        val result: () -> Unit = { tikXml.read<ServiceDeliveryDto>(bufferedSource, ServiceDeliveryDto::class.java) }
+
+        // ASSERTION
+        assertThrows(NullPointerException::class.java, result)
+    }
+
+    @Test
+    fun `Encode ServiceDelivery to string, encoding successful`() {
+        // GIVEN
+        val xmlFile = "src/test/resources/adapter/service/location_information_delivery.xml"
+        val bufferedSource = TestUtils().readXmlFile(xmlFile)
+        val result = tikXml.read<ServiceDeliveryDto>(bufferedSource, ServiceDeliveryDto::class.java)
+        assertThat(result).isNotNull()
+
+        // ACTION
+        val buffer = Buffer()
+        assertThat(buffer.size).isEqualTo(0)
+        tikXml.write(buffer, result)
+
+        // ASSERTION
+        assertThat(buffer.size).isGreaterThan(0)
+    }
+}
