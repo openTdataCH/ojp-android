b0a22e17970e66d0eaf9ddcc2bbb254d996a59f0
deka91 <denkal91@googlemail.com>
2024-05-27 10:33:10 +0200

    Set specific exception type to error classes

--- sdk/src/main/java/ch/opentransportdata/ojp/domain/model/error/OjpError.kt
+++ sdk/src/main/java/ch/opentransportdata/ojp/domain/model/error/OjpError.kt
@@ -1,5 +1,10 @@
 package ch.opentransportdata.ojp.domain.model.error
 
+import com.tickaroo.tikxml.XmlDataException
+import kotlinx.coroutines.CancellationException
+import retrofit2.HttpException
+import java.io.IOException
+
 /**
  * Created by Deniz Kalem on 06.05.2024
  */
@@ -8,19 +13,19 @@ sealed class OjpError : Error {
     data class NotImplemented(val exception: Exception) : OjpError()
 
     // When a response status code is != `200` this error is thrown
-    data class UnexpectedHttpStatus(val exception: Exception) : OjpError()
+    data class UnexpectedHttpStatus(val exception: HttpException) : OjpError()
 
     // A response is missing a required element. Eg. no `serviceDelivery` is present
-    data class MissingElement(val exception: Exception) : OjpError()
+    data class MissingElement(val exception: NullPointerException) : OjpError()
 
     // Issue when trying to generate a request xml
-    data class EncodingFailed(val exception: Exception) : OjpError()
+    data class EncodingFailed(val exception: IOException) : OjpError()
 
     // Can't correctly decode a XML response
-    data class DecodingFailed(val exception: Exception) : OjpError()
+    data class DecodingFailed(val exception: XmlDataException) : OjpError()
 
-    // Can't correctly decode a XML response
-    data class RequestCancelled(val exception: Exception) : OjpError()
+    // Job of the coroutine is cancelled while it is suspending
+    data class RequestCancelled(val exception: CancellationException) : OjpError()
 
     // Unknown error
     data class Unknown(val exception: Exception) : OjpError()
