e00430d3c68efb8ea2f66eed790fc3680106bd81
kalevan <michi.ruppen@gmail.com>
2024-09-27 14:06:34 +0200

    Use LocalDateTime to parse object

--- sdk/src/main/java/ch/opentransportdata/ojp/data/dto/response/tir/situations/PublishingActionsDto.kt
+++ sdk/src/main/java/ch/opentransportdata/ojp/data/dto/response/tir/situations/PublishingActionsDto.kt
@@ -5,6 +5,7 @@ import com.tickaroo.tikxml.annotation.Element
 import com.tickaroo.tikxml.annotation.PropertyElement
 import com.tickaroo.tikxml.annotation.Xml
 import kotlinx.parcelize.Parcelize
+import java.time.LocalDateTime
 
 /**
  * Created by Michael Ruppen on 28.06.2024
@@ -21,13 +22,13 @@ data class PublishingActionsDto(
 data class PublishingActionDto(
     @Element(name = "siri:PassengerInformationAction")
     val passengerInformationAction: PassengerInformationActionDto?
-): Parcelable
+) : Parcelable
 
 @Parcelize
 @Xml(name = "siri:PassengerInformationAction")
 data class PassengerInformationActionDto(
     @PropertyElement(name = "siri:RecordedAtTime")
-    val recordedAtTime: String?, //values like 00001-01-01T00:00:00 are returned and can not be parsed to LocalDateTime thus it's a String
+    val recordedAtTime: LocalDateTime?,
     @Element(name = "siri:TextualContent")
     val textualContent: TextualContentDto?
-): Parcelable
+) : Parcelable
