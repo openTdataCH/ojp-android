df2bf06a3664e4a81fc11ef640c4f6792b7cfce5
deka91 <denkal91@googlemail.com>
2025-03-05 15:04:25 +0100

    Fix transparent status bar

--- app/src/main/java/ch/opentransportdata/presentation/MainActivity.kt
+++ app/src/main/java/ch/opentransportdata/presentation/MainActivity.kt
@@ -3,13 +3,19 @@ package ch.opentransportdata.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,7 +24,10 @@ import androidx.navigation.toRoute
 import ch.opentransportdata.ojp.OjpSdk
 import ch.opentransportdata.ojp.data.dto.response.PlaceResultDto
 import ch.opentransportdata.presentation.lir.LirScreenComposable
-import ch.opentransportdata.presentation.navigation.*
+import ch.opentransportdata.presentation.navigation.BottomNavItem
+import ch.opentransportdata.presentation.navigation.LocationSearchMask
+import ch.opentransportdata.presentation.navigation.TripResults
+import ch.opentransportdata.presentation.navigation.TripSearchMask
 import ch.opentransportdata.presentation.navigation.navtypes.PlaceResultType
 import ch.opentransportdata.presentation.theme.OJPAndroidSDKTheme
 import ch.opentransportdata.presentation.tir.TirScreenComposable
@@ -29,6 +38,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent { OjpDemoApp() }
     }
 
@@ -42,6 +52,7 @@ class MainActivity : ComponentActivity() {
             var selectedBottomNavItem by remember { mutableIntStateOf(0) }
 
             Scaffold(
+                contentWindowInsets = WindowInsets(0),
                 bottomBar = {
                     NavigationBar {
                         bottomNavigationItems.forEachIndexed { index, item ->
--- app/src/main/java/ch/opentransportdata/presentation/theme/Theme.kt
+++ app/src/main/java/ch/opentransportdata/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ch.opentransportdata.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +9,9 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -273,14 +269,9 @@ fun OJPAndroidSDKTheme(
         darkTheme -> darkScheme
         else -> lightScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(Color.Transparent, darkIcons = true)
 
     MaterialTheme(
         colorScheme = colorScheme,
