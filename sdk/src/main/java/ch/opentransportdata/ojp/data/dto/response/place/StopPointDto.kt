15eaa4eee50cb6b1c05b5ee67f43c6098cd058cc
deka91 <denkal91@googlemail.com>
2025-03-26 14:02:39 +0100

    Implement extension function for find parent station

--- sdk/src/main/java/ch/opentransportdata/ojp/data/dto/response/place/StopPointDto.kt
+++ sdk/src/main/java/ch/opentransportdata/ojp/data/dto/response/place/StopPointDto.kt
@@ -37,4 +37,10 @@ data class StopPointDto(
     val lighting: Boolean?,
     @PropertyElement(name = "Covered")
     val covered: Boolean?
-) : AbstractPlaceDto()
+) : AbstractPlaceDto() {
+
+    fun List<StopPointDto>?.findParentStation(stopPointRef: String): String {
+        val match = this?.firstOrNull { it.stopPointRef == stopPointRef }
+        return match?.parentRef ?: stopPointRef
+    }
+}
