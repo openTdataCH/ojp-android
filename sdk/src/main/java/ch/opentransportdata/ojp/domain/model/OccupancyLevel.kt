45a0418f853fabf96de926401329e6614dca68b3
deka91 <denkal91@googlemail.com>
2025-06-25 15:39:43 +0200

    Implement enum for FareClass and OccupancyLevel

---
+++ sdk/src/main/java/ch/opentransportdata/ojp/domain/model/OccupancyLevel.kt
@@ -0,0 +1,12 @@
+package ch.opentransportdata.ojp.domain.model
+
+/**
+ * Created by Deniz Kalem on 25.06.2025
+ */
+enum class OccupancyLevel {
+    UNKNOWN,
+    MANY_SEATS_AVAILABLE,
+    FEW_SEATS_AVAILABLE,
+    STANDING_ROOM_ONLY,
+    FULL
+}
